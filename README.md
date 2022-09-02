ba3170407f7b75bc13bd1cab091e0de138905100
jjavierf64 <jjavierf64@estudiantec.cr>
2022-09-02 11:15:10 -0600

    First

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Cpp_Primeros_Pasos
+
+Función Main
+```cpp
+int main(){
+    return 0;
+}
+
+```
+Tipos de datos básicos
+```cpp
+//Comentarios
+int numeroEntero = 0;
+
+
+
+```
---
+++ datos.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int numero=0;
+    float flotante = 1.23;
+    double mayor = 1.2356;
+    char unaLetra = 'a';
+
+    return 0;
+}
